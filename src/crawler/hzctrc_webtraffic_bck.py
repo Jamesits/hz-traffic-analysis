fc05cbc1ea3855ba754518a9175d2eafb0154ec7
James Swineson <jamesswineson@gmail.com>
2016-07-11 19:41:11 +0800

    remove dup prefix

--- src/crawler/hzctrc_webtraffic_bck.py
+++ src/crawler/hzctrc_webtraffic_bck.py
@@ -7,7 +7,7 @@ import json
 import sys
 
 url = """http://www.hzctrc.com/arcgis/rest/services/V32/webtraffic_bck/MapServer/identify?f=json&geometry=%s&tolerance=1&returnGeometry=false&mapExtent={"xmin":120.000,"ymin":30.000,"xmax":121.000,"ymax":31.000,"spatialReference":{"wkid":4326}}&imageDisplay=400,400,96&geometryType=esriGeometryEnvelope&sr=4326&layers=all:0,1,2,3,4,5"""
-dataset = "hzctrc_webtraffic_bck"
+dataset = "webtraffic_bck"
 
 #                     xmin        ymin   xmax   ymax
 r = requests.get(url %"122.040,30.265,120.150,30.313")
